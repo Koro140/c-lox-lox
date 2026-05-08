218004622f3d07a7af08a24fff6916733d0b316d
Koro140 <ahmedhussien873@gmail.com>
2026-05-08 10:56:34 +0300

    Fixed the AstPrinter number problem

--- src/AstPrinter.h
+++ src/AstPrinter.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "Expr.h"
 
 class AstPrinter : public Visitor
@@ -5,19 +6,22 @@ class AstPrinter : public Visitor
 private:
     std::string result;
     
-    template<typename... TExpr>
     void parenthesize(std::string name, std::vector<Expr*> exprs) {
-
         result.append("(").append(name);
 
         for (Expr* expr : exprs) {
             expr->accept(*this);
-            std::cout << "he";
         }
 
         result.append(")");
     }
 public:
+
+    std::string print(Expr* expr) {
+        expr->accept(*this);
+        return result;
+    }
+
 	void visit(Grouping& v) {
         parenthesize("group", {v.expr});
     }
@@ -34,16 +38,11 @@ public:
         if (v.value.type() == typeid(nullptr)) {
             result.append("nil");
         }
-        else if (v.value.type() == typeid(double)){
+        else if (v.value.type() == typeid(double) || v.value.type() == typeid(int)){
             result.append(std::to_string(std::any_cast<double>(v.value)));
-            std::cout << std::any_cast<double>(v.value);
         }
         else if (v.value.type() == typeid(std::string)) {
             result.append(std::any_cast<std::string>(v.value));
         }
-    }
-
-    std::string getResult() {
-        return result;
     }
 };
